d39c3ac021846f458908f8d5285f6fe4867078f3
lkp25 <lkp25@o2.pl>
2021-06-02 18:33:23 +0200

    added keyboard input functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 import Calculator from "./Calculator.js"
 
-const NUMBER_FORMATTER = new Intl.NumberFormat("en", {maximumFractionDigits: 20})
+
 
 const calculator = new Calculator()
 calculator.clearDisplay()
@@ -25,4 +25,67 @@ document.addEventListener('click', (e)=>{
     }
 })
 
- 
+const allBtns =  [...document.querySelectorAll('button')]
+// allBtns.forEach(btn => console.log(btn.textContent))
+ document.addEventListener('keydown', (e)=>{
+     
+     switch (e.key) {
+         case '=':
+             allBtns.find(btn => btn.textContent === '=').click()
+             break;
+         case 'Delete':
+             allBtns.find(btn => btn.textContent === 'DEL').click()
+             break;
+         case 'c':
+             allBtns.find(btn => btn.textContent === 'AC').click()
+             break;
+         case '/':
+             allBtns.find(btn => btn.textContent === '/').click()
+             break;
+         case '*':
+             allBtns.find(btn => btn.textContent === '*').click()
+             break;
+         case '-':
+             allBtns.find(btn => btn.textContent === '-').click()
+             break;
+         case '+':
+             allBtns.find(btn => btn.textContent === '+').click()
+             break;
+         case '1':
+             allBtns.find(btn => btn.textContent === '1').click()
+             break;
+         case '2':
+             allBtns.find(btn => btn.textContent === '2').click()
+             break;
+         case '3':
+             allBtns.find(btn => btn.textContent === '3').click()
+             break;
+         case '4':
+             allBtns.find(btn => btn.textContent === '4').click()
+             break;
+         case '5':
+             allBtns.find(btn => btn.textContent === '5').click()
+             break;
+         case '6':
+             allBtns.find(btn => btn.textContent === '6').click()
+             break;
+         case '7':
+             allBtns.find(btn => btn.textContent === '7').click()
+             break;
+         case '8':
+             allBtns.find(btn => btn.textContent === '8').click()
+             break;
+         case '9':
+             allBtns.find(btn => btn.textContent === '9').click()
+             break;
+         case '0':
+             allBtns.find(btn => btn.textContent === '0').click()
+             break;
+         case '.':
+             allBtns.find(btn => btn.textContent === '.').click()
+             break;
+     
+         default:
+             break;
+     }
+ })

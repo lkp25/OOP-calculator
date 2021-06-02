f3934cdc16af331800d0788458ffb736fc0793e4
lkp25 <lkp25@o2.pl>
2021-06-02 15:01:08 +0200

    all button click listeners setup correctly

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,19 @@ import Calculator from "./Calculator.js"
 const calculator = new Calculator()
 
 document.addEventListener('click', (e)=>{
-    if(e.target.dataset.action.matches('clear')){
-        console.log('dupa');
+    if(e.target.matches('[data-input]')){
+        console.log('input');
+    }
+    if(e.target.matches('[data-operation]')){
+        console.log('oper');
+    }
+    if(e.target.matches('[data-evaluate]')){
+        console.log('eval');
+    }
+    if(e.target.matches('[data-delete]')){
+        console.log('del');
+    }
+    if(e.target.matches('[data-clear]')){
+        console.log('clea');
     }
 })
